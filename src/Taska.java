8185d76247c088539c87acdc5be4cfd8b5f702b3
shamagulov <shamagulovv@gmail.com>
2022-02-13 15:30:20 +0500

    initi commit

---
+++ src/Taska.java
@@ -0,0 +1,14 @@
+
+public class Taska {
+
+    public static String hello() {
+        return "Hello!";
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Taska taska = null;
+        System.out.println(taska.hello());
+    }
+}
